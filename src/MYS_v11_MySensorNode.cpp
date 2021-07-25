7bf31ccb22aef10b3335920a2bd4bd2e73754d7d
Richard Clarke <richard@clarke.biz>
2021-07-25 17:28:16 +1200

    Update MYS_v11_MySensorNode.cpp

--- src/MYS_v11_MySensorNode.cpp
+++ src/MYS_v11_MySensorNode.cpp
@@ -104,6 +104,8 @@ enum child_id_t
   CHILD_ID_EXT_VOLTAGE
 };
 
+std::map<child_id_t, std::string> m {{CHILD_ID_HUMIDITY,"Humidity"}, {CHILD_ID_TEMP,"Temperature"}, {CHILD_ID_UV,"UV"}, {CHILD_ID_VOLTAGE,"MCU Voltage"}, {CHILD_ID_EXT_VOLTAGE,"External Voltage"}};
+
 uint32_t clockSwitchCount = 0;
 
 /*****************************/
